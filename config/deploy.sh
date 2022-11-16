d5b16b95dc95e1ec556b3b834632e25ab2c9bba7
pooi <ldayou@naver.com>
2022-11-16 18:00:25 +0900

    Setup logback and rename beta to dev

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,9 +1,9 @@
-REPOSITORY=/home/ubuntu/housetainer/app/beta
+REPOSITORY=/home/ubuntu/housetainer/app/production
 cd $REPOSITORY
 
 APP_NAME=housetainer
 
-CURRENT_PID=$(pgrep -f $APP_NAME/application-beta.yaml)
+CURRENT_PID=$(pgrep -f $APP_NAME/application.yaml)
 
 if [ -z $CURRENT_PID ] #2
 then
@@ -18,4 +18,4 @@ JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/$JAR_NAME
 
 echo "> Deploy $JAR_PATH" #3
-nohup java -jar $JAR_PATH --spring.config.location=/home/ubuntu/housetainer/application-beta.yaml > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH --spring.config.location=/home/ubuntu/housetainer/application.yaml -Dlogging.config=logback.xml > /dev/null 2> /dev/null < /dev/null &
